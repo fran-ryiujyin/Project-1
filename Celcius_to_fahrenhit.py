0e99fecf906ac98f168d0d87be7404f42a3ebb64
fran-ryiujyin <111332507+fran-ryiujyin@users.noreply.github.com>
2024-03-02 11:22:12 +0700

    Add files via upload

---
+++ Celcius_to_fahrenhit.py
@@ -0,0 +1,4 @@
+#Convert Celcius to Fahrenheit
+input_number = int(input("Input Number = "))
+fahrenheit = (input_number * 9/5) + 32
+print(f"{input_number} Celcius is Equal With {fahrenheit} Fahrenheit")
